23725ce79936c3f776dd0f143321b3ebcc695748
vikash <vikashraj144@gmail.com>
2021-04-21 17:56:25 +0530

    chat component

--- chat-ui/src/components/chat/Chat.jsx
+++ chat-ui/src/components/chat/Chat.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../UserContext";
 
 const Chat = (props) => {
-  return <div>Chat</div>;
+  const { user, setUser } = useContext(UserContext);
+  return (
+    <div>
+      Chat {JSON.stringify(user)}
+      <Link to={"/"}>
+        <button>go to Home</button>
+      </Link>
+    </div>
+  );
 };
 
 Chat.propTypes = {};
