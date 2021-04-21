6f44b069167539d0c738dd935380e10b49130eb6
vikash <vikashraj144@gmail.com>
2021-04-21 09:09:34 +0530

    initial set up of chat

---
+++ chat-ui/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Chat from "./components/chat/Chat";
+import Home from "./components/home/Home";
+import { UserContext } from "./UserContext";
+
+function App() {
+  const [user, setUser] = useState(null);
+  return (
+    <Router>
+      <div>
+        <UserContext.Provider value={(user, setUser)}>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/chat">
+              <Chat />
+            </Route>
+          </Switch>
+        </UserContext.Provider>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ chat-ui/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = (props) => {
+  return <div>Home</div>;
+};
+
+Home.propTypes = {};
+
+export default Home;
