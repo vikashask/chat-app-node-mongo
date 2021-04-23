d6b217926b1ce43866002fe974ebe5fd02b82c7b
vikash <vikashraj144@gmail.com>
2021-04-23 09:25:42 +0530

    add user remove user

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+
+const { addUser, removeUser, getUser } = require("./helper.js");
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
@@ -18,13 +20,47 @@ const PORT = process.env.PORT || 3001;
 // use it before all route definitions
 app.use(cors({ origin: "http://localhost:3000" }));
 
-// app.get("/", (req, res) => {
-//   res.send("Welcome");
-//   // res.sendFile(__dirname + "/index.html");
-// });
-
 io.on("connection", (socket) => {
+  socket.on("create-room", (name) => {
+    console.log("🚀 ~ file: index.js ~ line 23 ~ io.on ~ name", name);
+  });
+
+  socket.on("join", ({ name, room_id, user_id }) => {
+    const { error, user } = addUser({
+      socket_id: socket.id,
+      name,
+      room_id,
+      user_id,
+    });
+    socket.join(room_id);
+
+    if (error) {
+      console.log("Join error", error);
+    } else {
+      console.log("join user");
+    }
+  });
   console.log("connected");
+
+  socket.on("sendMessage", (message, room_id, callback) => {
+    const user = getUser(socket.id);
+    const msgToStore = {
+      name: user.name,
+      user_id: user.user_id,
+      room_id,
+      text: message,
+    };
+    console.log(
+      "🚀 ~ file: index.js ~ line 51 ~ socket.on ~ msgToStore",
+      msgToStore
+    );
+    io.to(room_id).emit("message", msgToStore);
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+  });
 });
 
 server.listen(PORT, () => {
